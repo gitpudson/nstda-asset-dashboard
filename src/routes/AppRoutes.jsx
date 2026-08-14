f9a98d6659bb40a82b19db52e9b4da69932efe48
@pudson <pichet.pudson@nectec.or.th>
2026-08-14 16:57:43 +0700

    Update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Dashboard/Home";
@@ -6,13 +7,29 @@ import OrgDashboard from "../pages/Dashboard/OrgDashboard";
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
+    // <HashRouter>
+    //   <Routes>
+    //     <Route element={<MainLayout />}>
+    //       <Route path="/nstda-asset-dashboard" element={<Home />} />
+    //       <Route path="/nstda-asset-dashboard/org/:org" element={<OrgDashboard />} />
+    //     </Route>
+    //   </Routes>
+    // </HashRouter>
+
+    <HashRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/nstda-asset-dashboard" element={<Home />} />
-          <Route path="/nstda-asset-dashboard/org/:org" element={<OrgDashboard />} />
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/org/:org"
+            element={<OrgDashboard />}
+          />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
+
   );
 }
